a0d6018b99dadedbcc6bdb34621ded3dcfb9eb35
Damien Tanner <damien@tanner.me>
2018-05-10 13:00:31 +0100

    latest

--- getapp_scrapy/settings.py
+++ getapp_scrapy/settings.py
@@ -8,7 +8,10 @@ NEWSPIDER_MODULE = 'getapp_scrapy.spiders'
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
 
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+RANDOMIZE_DOWNLOAD_DELAY = False
+DOWNLOAD_DELAY = 1
 DOWNLOADER_MIDDLEWARES = {
     # The priority of 560 is important, because we want this middleware to kick in just before the scrapy built-in `RetryMiddleware`.
-    'scraping_hub.middlewares.CloudFlareMiddleware': 560
+    'getapp_scrapy.middlewares.CloudFlareMiddleware': 560
 }
--- getapp_scrapy/spiders/listings.py
+++ getapp_scrapy/spiders/listings.py
@@ -5,7 +5,7 @@ class ListingsSpider(scrapy.Spider):
     name = "listings"
     allowed_domains = ["getapp.com"]
     start_urls = [
-        'https://www.getapp.com/business-intelligence-analytics-software/analytics/',
+        'https://www.getapp.com/business-intelligence-analytics-software/big-data/',
     ]
 
     def parse(self, response):
@@ -19,16 +19,9 @@ class ListingsSpider(scrapy.Spider):
         item = {}
         listing = response.css("div.product_main")
 
-        item["name"] = response.css("h2 > a ::text").extract_first()
+        item["name"] = response.css("h2 ::text").extract_first()
         item["desc"] = response.css("p.lead > span ::text").extract_first()
         item["rating"] = response.css('span[itemprop="ratingValue"] ::text').extract_first()
         item["num_reviews"] = response.css('meta[itemprop="ratingCount"] ::attr(content)').extract_first()
-
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item["website_click_out"] = response.css('a.btn-primary[data-evca="ua_click_out"] ::attr(href)').extract_first()
         yield item
