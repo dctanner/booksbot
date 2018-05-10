267779f07a9a91956bbaff9392c9dde4c0d1228b
Damien Tanner <damien@tanner.me>
2018-05-10 13:27:03 +0100

    saves category url

--- getapp_scrapy/spiders/listings.py
+++ getapp_scrapy/spiders/listings.py
@@ -5,12 +5,12 @@ class ListingsSpider(scrapy.Spider):
     name = "listings"
     allowed_domains = ["getapp.com"]
     start_urls = [
-        'https://www.getapp.com/business-intelligence-analytics-software/big-data/',
+        'https://www.getapp.com/business-intelligence-analytics-software/data-mining/',
     ]
 
     def parse(self, response):
         for listing_url in response.css("a.serp-read-more ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(listing_url), callback=self.parse_listing_page)
+            yield scrapy.Request(response.urljoin(listing_url), callback=self.parse_listing_page, meta={'start_url':response.url})
         next_page = response.css("ul.pagination > li:last-child > a ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
@@ -24,4 +24,5 @@ class ListingsSpider(scrapy.Spider):
         item["rating"] = response.css('span[itemprop="ratingValue"] ::text').extract_first()
         item["num_reviews"] = response.css('meta[itemprop="ratingCount"] ::attr(content)').extract_first()
         item["website_click_out"] = response.css('a.btn-primary[data-evca="ua_click_out"] ::attr(href)').extract_first()
+        item["category"] = response.meta['start_url']
         yield item
